5d4a3c5120edb826c0f81bb68a76d78288f8f411
Lugaga <maureezgaga@gmail.com>
2019-06-14 14:15:25 +0300

    add setUp method to initialize the test cases

--- credentials_test.py
+++ credentials_test.py
@@ -1,3 +1,17 @@
 from unittest import TestCase, main
 from credentials import Credentials
 import pyperclip
+
+class TestCredentials(TestCase):
+    """
+    Defines test cases for the credentials class behaviours.
+
+    Args:
+        TestCase: A class that helps create the test cases.
+    """
+
+    def setUp(self):
+        """
+        Method that runs before the test cases.
+        """
+        self.new_cred = Credentials('github','Lugaga', 'tangodown!')
