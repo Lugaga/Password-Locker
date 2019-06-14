88414a1ef5b52e11ed4b9aeabedb95d0c12ac892
Lugaga <maureezgaga@gmail.com>
2019-06-14 22:24:44 +0300

    add register function to create new user

--- run.py
+++ run.py
@@ -1,7 +1,15 @@
 #!usr/bin/env python 3.6
+
 from user import User
 from credentials import Credentials
 import random
 
 
+def register_user(f_name, l_name, u_name, password):
+    """
+    Function that creates a new Password Locker user account.
+    """
+    new_user = User(f_name, l_name, u_name, password)
+    new_user.save_user()
+    
 
