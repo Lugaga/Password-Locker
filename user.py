49bc99f56b55a7e342e550b1a0347d49276dcdfa
Lugaga <maureezgaga@gmail.com>
2019-06-14 12:18:37 +0300

    add a method to save objects to the user list

--- user.py
+++ user.py
@@ -5,7 +5,6 @@ class User:
     user_list = []
 
     def __init__(self, first_name, last_name, username, password):
-
         """
         Defines properties for our objects.
 
@@ -14,9 +13,16 @@ class User:
             last_name: New user last name.
             username: New user username.
             password: New user password.
-            
+
         """
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
         self.password = password
+
+
+def save_user(self):
+    """
+    Method that saves objects to user_list.
+    """
+    self.user_list.append(self)
