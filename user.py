9ecfafa3cf2a408940b6a895122554cc1160c30c
Lugaga <maureezgaga@gmail.com>
2019-06-14 12:41:37 +0300

    add docstrings to the method that checks if a user exists

--- user.py
+++ user.py
@@ -21,16 +21,23 @@ class User:
         self.password = password
 
 
-def save_user(self):
-    """
-    Method that saves objects to user_list.
-    """
-    self.user_list.append(self)
+    def save_user(self):
+        """
+        Method that saves objects to user_list.
+        """
+        self.user_list.append(self)
 
 
-@classmethod
-def user_exists(cls, logged_user):
-    """
-    A Method that checks if the user exists or not
-    """
-    
+    @classmethod
+    def user_exists(cls, logged_user):
+        """
+        A Method that checks if the user exists or not
+        args:
+        logged_user: username to search if the user exists
+
+        returns:boolean:true or false depending on the condition
+        """
+        for user in cls.user_list:
+            if user.username == logged_user:
+                return True
+        return False
