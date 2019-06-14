4986d6215a929c456fc6e4a1e66dcb3369818d1e
Lugaga <maureezgaga@gmail.com>
2019-06-14 12:05:30 +0300

    create instance of the user class using self

--- user.py
+++ user.py
@@ -2,6 +2,8 @@ class User:
     """
     A class that generates new instances of users.
     """
-    user_list []
+    user_list = []
+    
+    def __init__(self, first_name, last_name, username, password):
     
     
